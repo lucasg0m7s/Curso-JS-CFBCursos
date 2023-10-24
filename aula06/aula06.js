1e3b04892ff5930b7820e45c2fcec0cc1ba196a0
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2023-10-24 18:44:20 -0300

    Add aula06

---
+++ aula06/aula06.js
@@ -0,0 +1,29 @@
+/*
+== considera o valor da variável e a posição na memória
+=== considera o valor, o tipo da variável e a posição na memória
+
+let n1 = 1
+let n2 = "1"
+
+let v1 = {
+    nome: "Bruno"
+}
+
+let v2 = v1
+
+console.log(v1 == v2)
+
+----------------------------------------
+
+let nome = prompt("Digite seu nome")
+
+console.log(nome)
+
+----------------------------------------
+
+let n1 = 1
+let n2 = 2
+
+console.log(!(n1 != n2))
+
+*/
