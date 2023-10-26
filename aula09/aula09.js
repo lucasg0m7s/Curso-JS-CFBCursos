69987e1ac89b202cc2e78a903910c4626dee62b1
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2023-10-25 22:56:20 -0300

    Add aula09

---
+++ aula09/aula09.js
@@ -0,0 +1,12 @@
+// Operadores de pré e pós incremento
+// n++ Primeiro usa o valor depois incrementa
+// ++n incrementa depois usa
+// O mesmo com --n ou n--
+// - é um operador de inversão
+// + com strings concatena e com numeros soma
+
+let n = 10
+let x = -n
+
+
+console.log(x)
