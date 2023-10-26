d7b8b55a694cc8480f08dfd1f57c7d9b37c757ff
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2023-10-25 22:39:43 -0300

    Add aula08

---
+++ aula08/aula08.js
@@ -0,0 +1,12 @@
+// Operadores bitwise
+// & retorna o bit 0 em caso de não equivalencia
+// | retorna o bit 1 em caso de não equivalencia
+// ^ Em caso de não equivalencia ele retorna 1 e equivalencia retorna 0
+// << 1 dobra o valor
+// >> 1 divide o valor pela metade
+
+let n1 = 10
+
+let res = n1 >> 1 
+
+console.log(res)
