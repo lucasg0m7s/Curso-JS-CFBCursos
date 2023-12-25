bf616319522836cd589f6a9645bc12f172a8c699
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2023-12-25 02:07:23 -0300

    aula25 concluída

---
+++ aula25-ArrowFunction/aula25.js
@@ -0,0 +1,22 @@
+// const soma = function(v1, v2){return v1 + v2}
+
+const soma = (v1,v2) =>{
+    let res = v1 + v2
+    return res
+}
+
+console.log(soma(10,5))
+
+const nome = n => n // em caso de apenas um parâmetro não precisa de parenteses
+console.log(nome("Bruno"))
+
+const add = n => n+10 // em funções com uma operação simples não preciso das chaves
+console.log(add(10))
+
+
+
+
+
+
+
+
