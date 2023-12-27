8b81f443167220c8b3b15c332576bf3d52eec5ea
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2023-12-26 23:29:33 -0300

    aula28 concluída

--- aula28-Map/aula28.js
+++ aula28-Map/aula28.js
@@ -1,1 +1,34 @@
 // Função map serve para iterar entre todos os elementos de uma coleção
+
+const converterInt = (e) => parseInt(e)
+const dobrar = (e) => e * 2
+
+let num = ['1', '2','3', '4', '5'].map(dobrar)
+
+console.log(num)
+
+
+//-----------------------------------
+
+// const el = document.getElementsByTagName("div")
+// const val = Array.prototype.map.call(el,({innerHTML})=>{ return innerHTML})
+// console.log(val)
+
+//-----------------------------------
+
+// let el = document.getElementsByTagName("div")
+// el = [...el]
+// el.map((elemento, indice)=>{
+//     elemento.innerHTML = "CFB CURSOS"
+//     console.log(elemento.innerHTML)
+// })
+
+//-----------------------------------
+
+// const cursos = ['HTML', 'CSS', 'Javascript', 'PHP', 'React']
+
+// let c = cursos.map((elemento, indice) =>{
+//     return elemento
+// })
+
+// console.log(c)
