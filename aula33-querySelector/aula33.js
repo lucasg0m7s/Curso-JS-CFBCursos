8d362e07e3518b1bab4442ca40045eee672c4751
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2023-12-28 00:26:57 -0300

    aula 33 concluída e correção no nome da aula30

--- aula33-querySelector/aula33.js
+++ aula33-querySelector/aula33.js
@@ -1,20 +1,33 @@
-// getElementsByClassName() retorna um HTML collection igual o getElementsByTagName()
+//querySelector() é mais dinâmico, ou seja, ele pode obter um elemento a partir de uma classe, tag ou id. Porém, ele retorna apenas um elemento, o primeiro que encontrar
+// O querySelectorAll() faz a mesma coisa que o querySelector(), porém ele retorna uma coleção do tipo NodeList com todos os elementos encontrados
 
-
+const divTodas = [...document.getElementsByTagName("div")]
 const cursosTodos = [...document.getElementsByClassName("curso")]
 const cursosC1 = [...document.getElementsByClassName("c1")]
 const cursosC2 = [...document.getElementsByClassName("c2")]
-const cursoEspecial = document.getElementsByClassName("curso")[6]
+const cursoEspecial = document.getElementById("c1")
 
-console.log(cursosTodos)
-console.log(cursosC1)
-console.log(cursosC2)
-console.log(cursoEspecial)
+// Obtendo todos os elementos div que tem o atributo class
+// const query_divTodas = [...document.querySelectorAll("div[class]")] 
 
+// Obtendo todos os elementos div
+// const query_divTodas = [...document.querySelectorAll("div")] 
 
-cursosC2.map((e)=>{
-    e.classList.add("destaque") // Adicionando uma classe aos elementos obtidos pelo getElementsByClassName
-})
+// Obtendo todos os elementos p que estão dentro de div
+const query_divTodas = [...document.querySelectorAll("div > p")] 
+
+// Obtendo todos os elementos com a classe cursos, para isso colocamos um '.' antes do nome da classe
+const query_cursosTodos = [...document.querySelectorAll(".curso")] 
+const query_cursosC1 = [...document.querySelectorAll(".c1,p")]
+const query_cursosC2 = [...document.querySelectorAll(".c2")]
+
+// Obtendo um elemento através do id, para isso colocamos uma '#' antes do nome do id
+const query_cursoEspecial = document.querySelector("#c1")
+// ou
+const query_cursoEspecialAll = document.querySelectorAll("#c1")[0]
+
+console.log(query_divTodas)
+
 
 
 
