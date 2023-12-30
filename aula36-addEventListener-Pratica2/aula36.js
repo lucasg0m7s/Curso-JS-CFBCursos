41d5e8590da658dcfafe7936bb185ca39d488da3
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2023-12-29 21:17:06 -0300

    aula36 concluída

--- aula36-addEventListener-Pratica2/aula36.js
+++ aula36-addEventListener-Pratica2/aula36.js
@@ -1,6 +1,6 @@
 const caixa1 = document.querySelector("#caixa1")
 const caixa2 = document.querySelector("#caixa2")
-const btn = document.querySelector("#btn_copiar")
+const btn_transferir = document.querySelector("#btn_transferir")
 const todosCursos = [...document.querySelectorAll(".curso")]
 
 todosCursos.map((el)=>{
@@ -10,9 +10,15 @@ todosCursos.map((el)=>{
     })
 })
 
-btn.addEventListener("click", ()=>{
+btn_transferir.addEventListener("click", ()=>{
     const cursosSelecionados = [...document.querySelectorAll(".selecionado")]
+    const cursosNaoSelecionados = [...document.querySelectorAll(".curso:not(.selecionado)")]
+    
     cursosSelecionados.map((el)=>{
         caixa2.appendChild(el) // Cria um elemento filho dentro do chamador do metodo appendChild()
+    })
+
+    cursosNaoSelecionados.map((el)=>{
+        caixa1.appendChild(el) // Cria um elemento filho dentro do chamador do metodo appendChild()
     })
 })
