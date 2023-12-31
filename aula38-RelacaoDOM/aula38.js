862bc440aaf9c253fb4cf67f1e9b9c4df9ccde69
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2023-12-31 16:12:13 -0300

    aula38 criada

---
+++ aula38-RelacaoDOM/aula38.js
@@ -0,0 +1,16 @@
+// O método stopPropagation() para a propagação de um evento criado no elemento pai, em seus filhos.
+
+const caixa1 = document.querySelector("#caixa1")
+const btn_c1 = document.querySelector("#c1")
+const cursos = [...document.querySelectorAll(".curso")]
+
+caixa1.addEventListener("click",(evt)=>{
+    console.log('clicou');
+})
+
+cursos.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        evt.stopPropagation()
+    })
+})
+
