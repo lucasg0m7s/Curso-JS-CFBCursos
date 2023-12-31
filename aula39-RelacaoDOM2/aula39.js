b2b9ea68d9766df5629520746642d895df4fe370
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2023-12-31 17:04:38 -0300

    aula39 criada

---
+++ aula39-RelacaoDOM2/aula39.js
@@ -0,0 +1,11 @@
+const caixa1 = document.querySelector("#caixa1")
+const btn_c = [...document.querySelectorAll(".curso")]
+
+// console.log(caixa1.firstElementChild)
+// console.log(caixa1.lastElementChild)
+// console.log(caixa1.children)
+
+console.log(btn_c[0].getRootNode())
+console.log(btn_c[0].ownerDocument)
+
+
