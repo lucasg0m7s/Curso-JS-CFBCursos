af954d8937803c671a9d5064efa895fd5d91fa56
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2023-12-31 23:44:07 -0300

    aula41 concluída

--- aula41-RemovendoDOM/aula41.js
+++ aula41-RemovendoDOM/aula41.js
@@ -1,5 +1,3 @@
-// Criando elementos
-
 const caixa1 = document.querySelector("#caixa1")
 const btn_c = [...document.querySelectorAll(".curso")]
 const c1_2 = document.querySelector("#c1_2")
@@ -11,13 +9,18 @@ cursos.map((el, chave)=>{
     novoElemento.innerHTML = el
     novoElemento.setAttribute("id", "c" + chave)
     novoElemento.setAttribute("class", "curso c1")
+
+    const btn_lixeira = document.createElement("img")
+    btn_lixeira.setAttribute("src", "iconlixeira.png")
+    btn_lixeira.setAttribute("class", "btn_lixeira")
+    btn_lixeira.addEventListener("click", (evt)=>{
+        caixa1.removeChild(evt.target.parentNode) // Remove Elementos
+    })
+
+    novoElemento.appendChild(btn_lixeira)
     caixa1.appendChild(novoElemento)
 })
 
-// const novoElemento = document.createElement("div")
-// novoElemento.setAttribute("id", "c7")
-// novoElemento.setAttribute("class", "curso c1")
-// novoElemento.innerHTML = "ReactNative"
 
 
 
