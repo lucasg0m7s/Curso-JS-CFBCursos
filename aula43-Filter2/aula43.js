ed74ba86992d7a4b57dc381a384914b005ef3298
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-01 16:20:48 -0300

    aula43 concluída

--- aula43-Filter2/aula43.js
+++ aula43-Filter2/aula43.js
@@ -1,26 +1,45 @@
-// Método filter usado para operar os elementos de um array e retornar apenas os que passarem no filtro 
+const caixaCursos = document.querySelector("#caixaCursos")
+const btn_c = [...document.querySelectorAll(".curso")]
+const c1_2 = document.querySelector("#c1_2")
+const cursos = ["HTML", "CSS", "Javascript", "PHP", "React", "MySQL", "ReactNative"]
+const btnCursoSelecionado = document.getElementById("btnCursoSelecionado")
 
-// const filtroMaior18 = (valor, indice, array)=>{
-const filtroMaior18 = (valor)=>{
-    if(valor >= 18)
-        return valor 
-}    
+cursos.map((el, chave)=>{
+    const novoElemento = document.createElement("div")
+    novoElemento.setAttribute("id", "c" + chave)
+    novoElemento.setAttribute("class", "curso c1")
+    novoElemento.innerHTML = el
 
-const idades = [15,21,30,17,18,44,12,50]
+    const comandos = document.createElement("div")
+    comandos.setAttribute("class", "comandos")
 
-const maior = idades.filter((valor)=>{
-    if(valor >= 18)
-        return valor 
+    const rb = document.createElement("input")
+    rb.setAttribute("type", "radio")
+    rb.setAttribute("name", "rb_curso")
+
+    comandos.appendChild(rb)
+
+    novoElemento.appendChild(comandos)
+
+    caixaCursos.appendChild(novoElemento)
+
+})  
+
+btnCursoSelecionado.addEventListener("click", (evt)=>{
+    const todosRadios = [...document.querySelectorAll("input[type = radio]")]
+    let radioSelecionado = todosRadios.filter((elemento)=>{
+        return elemento.checked
+    })
+    radioSelecionado = radioSelecionado[0]
+    // const cursoSelecionado = radioSelecionado.parentNode.parentNode.firstChild.textContent
+    const cursoSelecionado = radioSelecionado.parentNode.previousSibling.textContent
+
+    alert("Curso selecionado: " + cursoSelecionado)
+
+    // console.log(todosRadios)
+    // console.log(radioSelecionado)
+    // console.log(cursoSelecionado)
 })
-
-const menor = idades.filter((valor)=>{
-    if(valor < 18)
-        return valor 
-})
-
-console.log(idades)
-console.log(maior)
-console.log(menor)
 
 
 
