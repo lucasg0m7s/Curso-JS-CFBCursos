ab7d6725f78a4988b3265cdd9f1771b930df0c71
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-03 20:52:33 -0300

    Concluir aula49

--- aula49-MetodoSome/aula49.js
+++ aula49-MetodoSome/aula49.js
@@ -1,15 +1,15 @@
-// Retorna true ou false de acordo com a regra que passarmos ao método. Sua função é iterar por todos os elementos do array e verificar a condição, se todos corresponderem retorna true, se apenas um não corresponder retorna false
+// Retorna true se um ou mais elementos coincidirem com a condição
 
 const txt_pesquisar = document.getElementById("txt_pesquisar")
 const p_array = document.getElementById("array")
 const btnVerificar = document.getElementById("btnVerificar")
 const resultado = document.getElementById("resultado")
 
-const elementos_array = [21,25,19,20,19,18,22]
+const elementos_array = [16,12,10,17,15,13,19]
 p_array.innerHTML = "[" + elementos_array + "]"
 
 btnVerificar.addEventListener("click", (evt)=>{
-    const retorno = elementos_array.every((e, i)=>{
+    const retorno = elementos_array.some((e, i)=>{
         if(e < 18){
             resultado.innerHTML = "Array não conforme na posição " + i
         }
