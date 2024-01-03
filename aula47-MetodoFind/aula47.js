be32ce7743285ae0e5fac3e660557241a82d844d
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-02 22:25:03 -0300

    Concluir aula47

--- aula47-MetodoFind/aula47.js
+++ aula47-MetodoFind/aula47.js
@@ -1,89 +1,20 @@
-const caixaCursos = document.querySelector("#caixaCursos")
-const btn_c = [...document.querySelectorAll(".curso")]
-const c1_2 = document.querySelector("#c1_2")
-const cursos = ["HTML", "CSS", "Javascript", "PHP", "React", "MySQL", "ReactNative"]
-const btnCursoSelecionado = document.getElementById("btnCursoSelecionado")
-const btnRemoverCurso = document.getElementById('btnRemoverCurso')
-const btnAdicionarNovoCursoAntes = document.getElementById("btnAdicionarNovoCursoAntes")
-const btnAdicionarNovoCursoDepois = document.getElementById("btnAdicionarNovoCursoDepois")
-const nomeCurso = document.getElementById("nomeCurso")
+// Método Find() pesquisa um elemento em um array e retorna true se achar e caso não ache retorna undefined 
 
-let indice = 0
+const txt_pesquisar = document.getElementById("txt_pesquisar")
+const p_array = document.getElementById("array")
+const btnPesquisar = document.getElementById("btnPesquisar")
+const resultado = document.getElementById("resultado")
 
-const tirarSelecao = ()=>{
-    const cursosSelecionados = [...document.querySelectorAll(".selecionado")]
-    cursosSelecionados.map((el)=>{
-        el.classList.remove("selecionado")
-    })
+const elementos_array = ["html", "css", "js"]
+p_array.innerHTML = "[" + elementos_array + "]"
 
-}
-
-const criarNovoCurso = (curso)=>{
-    const novoElemento = document.createElement("div")
-    novoElemento.setAttribute("id", "c" + indice)
-    novoElemento.setAttribute("class", "curso c1")
-    novoElemento.innerHTML = curso
-    novoElemento.addEventListener("click", (evt)=>{
-        tirarSelecao()
-        evt.target.classList.toggle("selecionado")
-    })
-
-    return novoElemento
-}
-
-cursos.map((el)=>{
-    const novoElemento = criarNovoCurso(el)
-    caixaCursos.appendChild(novoElemento)
-    indice++;
-})  
-
-const cursoSelecionado = ()=>{
-    const cursosSelecionados = [...document.querySelectorAll(".selecionado")]
-
-    return cursosSelecionados[0]
-}
-
-btnCursoSelecionado.addEventListener("click", (evt)=>{
-    try{
-        alert("Curso selecionado: " + cursoSelecionado().innerHTML)
-    }catch(ex){
-        alert("Selecione um curso")
-    }
-})
-
-btnRemoverCurso.addEventListener("click", (evt)=>{
-    const cs = cursoSelecionado();
-    if(cs != undefined){
-        cs.remove()
-    }else{
-        alert("Selecione um curso")
-    }
-})
-
-btnAdicionarNovoCursoAntes.addEventListener("click", (evt)=>{
-    try{
-        if(nomeCurso.value != ""){
-            const novoCurso = criarNovoCurso(nomeCurso.value)
-            caixaCursos.insertBefore(novoCurso, cursoSelecionado())
-        }else{
-            alert("Digite o nome do curso")
+btnPesquisar.addEventListener("click", (evt)=>{
+    resultado.innerHTML = "Valor não encontrado"
+    const retorno = elementos_array.find((e, i)=>{
+        if(e == txt_pesquisar.value.toLowerCase()){
+            resultado.innerHTML = "Valor encontrado " + e + " na posição " + i
+            return e
         }
-    }catch(ex){
-        alert("Selecione um curso")
-    }
+    })
+    console.log(retorno)
 })
-
-btnAdicionarNovoCursoDepois.addEventListener("click", (evt)=>{
-    try{
-        if(nomeCurso.value != ""){
-            const novoCurso = criarNovoCurso(nomeCurso.value)
-            caixaCursos.insertBefore(novoCurso, cursoSelecionado().nextSibling)
-        }else{
-            alert("Digite o nome do curso")
-        }   
-    }catch(ex){
-        alert("Selecione um curso")
-    }
-})
-
-
