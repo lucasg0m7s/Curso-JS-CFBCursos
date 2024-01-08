88d4810b1c0040a06b046c1469df03142a170bde
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-07 22:36:20 -0300

    Concluir aula54

--- aula54-ColecaoMap/aula54.js
+++ aula54-ColecaoMap/aula54.js
@@ -1,2 +1,32 @@
+// Coleção Map chave + elemento
+
 const caixa = document.querySelector("#caixa")
 
+let mapa = new Map()
+
+mapa.set("curso", "Javascript")
+mapa.set(10 , "CFB Cursos")
+mapa.set(1 , 100)
+mapa.set("canal" , 100)
+
+mapa.delete(1)
+
+console.log(mapa)
+
+let pes = 10
+let res = ""
+if(mapa.has(pes))
+{
+    res = "A chave existe na coleção com o valor: " + mapa.get(pes)
+}
+else
+{
+    res = "A chave não existe na coleção"
+}
+
+res += "<br/> O tamanho da coleção é " + mapa.size
+caixa.innerHTML = res
+
+mapa.forEach((el)=>{
+    console.log(el)
+})
