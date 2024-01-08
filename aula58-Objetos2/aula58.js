fa5cfcba65353be84f64aac6c28235cd8606a465
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-08 20:17:27 -0300

    Criar aula58

---
+++ aula58-Objetos2/aula58.js
@@ -0,0 +1,14 @@
+class Pessoa{
+    constructor(pnome){
+        this.nome = pnome
+    }
+}
+
+let p1 = new Pessoa("Bruno")
+let p2 = new Pessoa("Lucas")
+let p3 = new Pessoa("Miguel")
+
+
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
