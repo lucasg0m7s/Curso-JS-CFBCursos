ed59774d6239b3d39740455217caf66bc7df4934
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-07 22:50:35 -0300

    Criar aula56

---
+++ aula56-TemplateString/aula56.js
@@ -0,0 +1,9 @@
+// Template Strings
+
+const caixa = document.querySelector("#caixa")
+
+const curso = "Js"
+const canal = "CFB Cursos"
+const frase = "Este é o curso de " + curso + " do canal " + canal
+
+caixa.innerHTML = frase
