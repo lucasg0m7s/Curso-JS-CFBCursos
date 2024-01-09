613abbc2a09df9bb6d31e44a3b0bf529f426509f
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-08 22:37:49 -0300

    Concluir aula59

--- aula59-Objetos3/aula59.js
+++ aula59-Objetos3/aula59.js
@@ -1,74 +1,54 @@
-class Carro {
-    canal = "CFB Cursos" // Propriedade Padrão 
-
-    constructor(pnome, ptipo) {
+class Pessoa {
+    constructor(pnome, pidade) {
         this.nome = pnome
-        this.canal = "Youtube"
-        if (ptipo == 1) {
-            this.tipo = "Esportivo"
-            this.velMax = 300
-        }
-        else if (ptipo == 2) {
-            this.tipo = "Utilitário"
-            this.velMax = 100
-        }
-        else if (ptipo == 3) {
-            this.tipo = "Passeio"
-            this.velMax = 160
-        }
-        else {
-            this.tipo = "Militar"
-            this.velMax = 180
-        }
+        this.idade = pidade
     }
 
     getNome() {
         return this.nome
     }
 
-    getTipo() {
-        return this.tipo
+    getIdade() {
+        return this.idade
     }
 
-    getVelMax() {
-        return this.velMax
-    }
-
-    getInfo() {
-        return [this.nome, this.tipo, this.velMax]
-    }
-
-    setNome(nome){
+    setNome(nome) {
         this.nome = nome
     }
 
-    setTipo(tipo){
-        this.tipo = tipo
-    }
-
-    setVelMax(velMax){
-        this.velMax = velMax
+    setIdade(tipo) {
+        this.idade = idade
     }
 
     info() {
         console.log(`Nome: ${this.nome}`)
-        console.log(`Tipo: ${this.tipo}`)
-        console.log(`Velocidade Máxima: ${this.velMax}`)
-        console.log(`Canal: ${this.canal}`)
+        console.log(`Idade: ${this.idade}`)
         console.log("--------------------------")
     }
 }
 
-let c1 = new Carro("Rapidão", 1)
-let c2 = new Carro("Super Luxo", 3)
-let c3 = new Carro("Bombadão", 4)
-let c4 = new Carro("Carrego Tudo", 2)
+let pessoas = []
 
-c1.setNome("Super Veloz")
-c1.setTipo("Antigo")
-c1.setVelMax(500)
+const btn_add = document.querySelector("#btn_add")
+const res = document.querySelector(".res")
 
-c1.info()
-// c2.info()
-// console.log(c1.getInfo()[0])
+const addPessoa = ()=>{
+    res.innerHTML = ""
+    pessoas.map((p)=>{
+        const div = document.createElement("div")
+        div.setAttribute("class", "pessoa")
+        div.innerHTML = `Nome: ${p.getNome()}<br/>Idade: ${p.getIdade()}`
+        res.appendChild(div)
+    })
+}
 
+btn_add.addEventListener("click", (evt) => {
+    const nome = document.querySelector('#f_nome')
+    const idade = document.querySelector('#f_idade')
+    const p = new Pessoa(nome.value, idade.value);
+    pessoas.push(p)
+    nome.value = ""
+    idade.value = ""
+    nome.focus()
+    addPessoa()
+})
