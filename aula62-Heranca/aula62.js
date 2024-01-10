01c18fc05cc327ee082a429d8a87d19d9234f0fb
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-10 01:19:16 -0300

    Concluir aula62

--- aula62-Heranca/aula62.js
+++ aula62-Heranca/aula62.js
@@ -1,29 +1,71 @@
-const Pessoa={
-    nome: "",
-    idade: "",
-    getNome: function(){
-        return this.nome
-    },
-    getIdade: function(){
-        return this.idade
-    },
-    setNome: function(nome){
+// Herança acontece quando uma classe herda outra classe, obtendo os métodos e propriedades da classe pai
+
+class Carro{ // Classe Pai
+    constructor(nome, portas){
         this.nome = nome
-    },
-    setIdade: function(idade){
-        this.idade = idade
+        this.portas = portas
+        this.ligado = false
+        this.vel = 0
+        this.cor = undefined
+    }
+
+    ligar = function(){
+        this.ligado = true
+    }
+
+    desligado = function(){
+        this.ligado = false
+    }
+
+    setCor = function(cor){
+        this.cor = cor
     }
 }
 
-const res = document.querySelector(".res")
-const btn_add = document.getElementById("btn_add")
+class Militar extends Carro{ // Classe Filho
+    constructor(nome, portas, blindagem, municao){
+        super(nome, portas)
+        this.blindagem = blindagem
+        this.municao = municao
+        this.setCor("Verde")
+    }
+    atirar = function(){
+        if(this.municao > 0){
+            this.municao--
+        }
+    }
+}
 
-btn_add.addEventListener("click", ()=>{
-    Pessoa.setNome(document.getElementById("f_nome").value)
-    Pessoa.setIdade(document.getElementById("f_idade").value)
+class Utilitario extends Carro{
+    constructor(nome, portas, lugares){
+        super(nome, portas)
+        this.lugares = lugares
+    }
+}
 
-    const div = document.createElement("div")
-    div.setAttribute("class", "pessoa")
-    div.innerHTML = `Nome: ${Pessoa.getNome()}<br/>Idade: ${Pessoa.getNome()}`
-    res.appendChild(div)
-})
+const c1 = new Carro("Normal", 4)
+c1.ligar()
+c1.setCor("Preto")
+
+const c2 = new Militar("Lutador", 1, 100, 50)
+c2.setCor("Azul")
+c2.atirar()
+c2.atirar()
+c2.atirar()
+c2.atirar()
+
+console.log(`Nome ${c1.nome}`)
+console.log(`Portas ${c1.portas}`)
+console.log(`Ligado ${c1.ligado ? "Sim" : "Não"}`)
+console.log(`Velocidade ${c1.vel}`)
+console.log(`Cor ${c1.cor}`)
+console.log(`----------------------------------------`)
+
+console.log(`Nome ${c2.nome}`)
+console.log(`Portas ${c2.portas}`)
+console.log(`Ligado ${c2.ligado ? "Sim" : "Não"}`)
+console.log(`Velocidade ${c2.vel}`)
+console.log(`Blindagem ${c2.blindagem}`)
+console.log(`Munição ${c2.municao}`)
+console.log(`Cor ${c2.cor}`)
+console.log(`----------------------------------------`)
