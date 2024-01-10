88a52550896d0938bde5c8e83bdb3fd0b99ceac2
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-10 00:00:00 -0300

    Concluir aula61

--- aula61-Objetos5/aula61.js
+++ aula61-Objetos5/aula61.js
@@ -1,52 +1,29 @@
-function Pessoa(pnome, pidade){
-    this.nome = pnome,
-    this.idade = pidade,
-
-    this.getNome = function() {
+const Pessoa={
+    nome: "",
+    idade: "",
+    getNome: function(){
         return this.nome
     },
-
-    this.getIdade = function() {
+    getIdade: function(){
         return this.idade
     },
-
-    this.setNome = function(nome) {
+    setNome: function(nome){
         this.nome = nome
     },
-
-    this.setIdade = function(tipo) {
+    setIdade: function(idade){
         this.idade = idade
-    },
-
-    this.info= function() {
-        console.log(`Nome: ${this.nome}`)
-        console.log(`Idade: ${this.idade}`)
-        console.log("--------------------------")
     }
 }
 
-let pessoas = []
-
-const btn_add = document.querySelector("#btn_add")
 const res = document.querySelector(".res")
+const btn_add = document.getElementById("btn_add")
 
-const addPessoa = ()=>{
-    res.innerHTML = ""
-    pessoas.map((p)=>{
-        const div = document.createElement("div")
-        div.setAttribute("class", "pessoa")
-        div.innerHTML = `Nome: ${p.getNome()}<br/>Idade: ${p.getIdade()}`
-        res.appendChild(div)
-    })
-}
+btn_add.addEventListener("click", ()=>{
+    Pessoa.setNome(document.getElementById("f_nome").value)
+    Pessoa.setIdade(document.getElementById("f_idade").value)
 
-btn_add.addEventListener("click", (evt) => {
-    const nome = document.querySelector('#f_nome')
-    const idade = document.querySelector('#f_idade')
-    const p = new Pessoa(nome.value, idade.value);
-    pessoas.push(p)
-    nome.value = ""
-    idade.value = ""
-    nome.focus()
-    addPessoa()
+    const div = document.createElement("div")
+    div.setAttribute("class", "pessoa")
+    div.innerHTML = `Nome: ${Pessoa.getNome()}<br/>Idade: ${Pessoa.getNome()}`
+    res.appendChild(div)
 })
