aa6a8f345c5879ac454e7cd605298d47a1561bdd
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-16 14:33:13 -0300

    Concluir aula71

--- aula71-CalculadoraParte2/aula71.js
+++ aula71-CalculadoraParte2/aula71.js
@@ -1,99 +1,23 @@
-// Classes abstratas não podem ser instânciadas, apenas herdadas por outras classes
+const teclasNum = [...document.querySelectorAll(".num")] 
+const teclasOp = [...document.querySelectorAll(".op")]
+const teclaRes = document.querySelector(".res")
+const display = document.querySelector("#display")
+const tOn = document.getElementById("tOn")
+const tLimpar = document.getElementById("tLimpar")
 
-class CarroPadrao{
-    constructor(){
-        if(this.constructor === CarroPadrao){
-            throw new TypeError("Esta classe não pode ser instânciada")
-        }
-        if(this.ligar === undefined){
-            throw new TypeError("É obrigatório implementar o método ligar")
-        }
-        if(this.desligar === undefined){
-            throw new TypeError("É obrigatório implementar o método desligar")
-        }
-            this.rodas = 4
-            this.portas = 4
-            this.ligado = false
-        
-        
-    }
 
-}
+teclasNum.forEach((el)=>{
+    el.addEventListener("click",(evt)=>{
+        display.innerHTML += evt.target.innerHTML
+    })
+})
 
-class Carro extends CarroPadrao{
-    constructor(tipo, estagioTurbo){
-        super()
-        this.turbo = new Turbo(estagioTurbo)
-        if(tipo == 1){
-            this.velMax = 120 
-            this.nome = "normal"
-        }else if(tipo == 2){
-            this.velMax = 160 
-            this.nome = "esportivo"
-        }else if(tipo == 3){
-            this.velMax = 200 
-            this.nome = "super esportivo"
-        }
-        this.velMax += this.turbo.pot
-    }
-    info(){
-        console.log(this.nome)
-        console.log(this.velMax)
-        console.log(this.turbo)
-        console.log(this.rodas)
-        console.log(this.portas)
-        console.log(this.ligado)
-        console.log("--------------------------------")
-    }
-    
-    ligar(){
-        this.ligado = true
-    }
+teclasOp.forEach((el)=>{
+    el.addEventListener("click",(evt)=>{
+        display.innerHTML += evt.target.innerHTML
+    })
+})
 
-    desligar(){
-        this.ligado = false
-    }
-}
-
-class Turbo{
-    constructor(e){
-        if(e == 1){
-            this.pot = 50
-        }
-        else if(e == 2){
-            this.pot = 75
-        }
-        else if(e == 3){
-            this.pot = 100
-        }else{
-            this.pot = 0
-        }
-    }
-}
-
-class CarroEspecial extends Carro{
-    constructor(estagioTurbo){
-        super(4, estagioTurbo)
-        this.velMax = 300 + this.turbo.pot
-        this.nome = "Carro Especial"
-        this.tipoInfo = 1
-    }
-    info(){
-        if(this.tipoInfo == 1){
-            super.info()
-        }else{
-            console.log(`Nome...: ${this.nome}`)
-            console.log(`VelMax: ${this.velMax}`)
-            console.log(`Turbo: ${this.turbo.pot}`)
-            console.log("--------------------------------")
-        }
-    }
-}
-
-const c1 = new Carro(1, 0)
-const c2 = new Carro(1, 1)
-const c3 = new CarroEspecial(3)
-
-c1.info()
-c2.info()
-c3.info()
+tLimpar.addEventListener("click",()=>{
+    display.innerHTML = 0
+})
