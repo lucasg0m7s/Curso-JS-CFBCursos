f8ae7d40a67edf022427a2632ddf16ce0076f97a
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-17 22:18:37 -0300

    Concluir aula72

--- aula72-CalculadoraParte3/aula72.js
+++ aula72-CalculadoraParte3/aula72.js
@@ -4,20 +4,63 @@ const teclaRes = document.querySelector(".res")
 const display = document.querySelector("#display")
 const tOn = document.getElementById("tOn")
 const tLimpar = document.getElementById("tLimpar")
+const tIgual = document.getElementById("tIgual")
 
+let sinal = false
+let decimal = false
 
 teclasNum.forEach((el)=>{
     el.addEventListener("click",(evt)=>{
-        display.innerHTML += evt.target.innerHTML
+        sinal = false
+        if(evt.target.innerHTML == ",")
+        {
+            if(!decimal){
+                decimal = true
+                if(display.innerHTML == "0")
+                {
+                    display.innerHTML = "0,"
+                }
+                else
+                {
+                    display.innerHTML += evt.target.innerHTML
+                }
+            }
+        }
+        else
+        {
+            if(display.innerHTML == "0"){
+                display.innerHTML = ""
+            }
+            display.innerHTML += evt.target.innerHTML
+        }
     })
 })
 
 teclasOp.forEach((el)=>{
     el.addEventListener("click",(evt)=>{
-        display.innerHTML += evt.target.innerHTML
+        if(!sinal){
+            sinal = true 
+            if(display.innerHTML == "0"){
+                display.innerHTML = ""
+            }
+            if(evt.target.innerHTML == "X"){
+                display.innerHTML += "*"
+            }else{
+                display.innerHTML += evt.target.innerHTML
+            }
+        }
     })
 })
 
 tLimpar.addEventListener("click",()=>{
+    sinal = false
+    decimal = false
     display.innerHTML = 0
+})
+
+tIgual.addEventListener("click",()=>{
+    sinal = false
+    decimal = false
+    const res = eval(display.innerHTML)
+    display.innerHTML = res
 })
