2c7c7b4e0f7e6e09c943d38095f8eb100971ca78
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-19 18:09:17 -0300

    Concluir aula74

--- aula74-AbaEmGaveta/aula74.js
+++ aula74-AbaEmGaveta/aula74.js
@@ -6,6 +6,9 @@ const tCopy = document.getElementById("tCopy")
 const tLimpar = document.getElementById("tLimpar")
 const tIgual = document.getElementById("tIgual")
 const teste = document.getElementById("teste")
+const calc_aba = document.getElementById("calc_aba")
+const calc = document.getElementById("calc")
+const img_aba_calc = document.getElementById("img_aba_calc")
 
 let sinal = false
 let decimal = false
@@ -71,4 +74,14 @@ tCopy.addEventListener("click",()=>{
     // teste.select()
     // teste.setSelectionRange(0,99999) // Mobile
     // navigator.clipboard.writeText(teste.value)
+})
+
+calc_aba.addEventListener("click",(evt)=>{
+    calc.classList.toggle("calc_exibir")
+    console.log(evt.target)
+    if(calc.classList.contains("calc_exibir")){
+        img_aba_calc.setAttribute("src", "arrow_left.svg")
+    }else{
+        img_aba_calc.setAttribute("src", "arrow_right.svg")
+    }
 })
