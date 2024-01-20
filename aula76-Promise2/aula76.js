96baa23961a795f7df77343a559d3e3f50511ba1
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-19 21:38:23 -0300

    Criar aula76

---
+++ aula76-Promise2/aula76.js
@@ -0,0 +1,32 @@
+// Promise é uma promessa de execução que vai retornar algo que estiver pronto, enquanto não estiver a aplicação continua a trabalhar
+
+const numero = document.getElementById("numero")
+
+let promise = new Promise((resolve, reject)=>{
+    let resultado = true
+    let tempo = 3000
+
+    setTimeout(()=>{
+        if(resultado){
+            resolve("Deu tudo certo")
+        }
+        else
+        {
+            reject("Deu tudo errado")
+        }
+    },tempo)
+})
+
+promise.then((retorno)=>{
+    numero.innerHTML = retorno
+    numero.classList.remove("erro")
+    numero.classList.add("ok")
+})
+
+promise.catch((retorno)=>{
+    numero.innerHTML = retorno
+    numero.classList.add("erro")
+    numero.classList.remove("ok")
+})
+
+numero.innerHTML = "Processando..."
