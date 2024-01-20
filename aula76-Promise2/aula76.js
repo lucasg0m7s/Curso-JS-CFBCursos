c06228c84f1b79ba8d95653adff9b0697c78463d
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-19 22:19:39 -0300

    Concluir aula76

--- aula76-Promise2/aula76.js
+++ aula76-Promise2/aula76.js
@@ -1,32 +1,42 @@
-// Promise é uma promessa de execução que vai retornar algo que estiver pronto, enquanto não estiver a aplicação continua a trabalhar
-
 const numero = document.getElementById("numero")
+const btn_promessa = document.getElementById("btn_promessa")
 
-let promise = new Promise((resolve, reject)=>{
-    let resultado = true
-    let tempo = 3000
-
-    setTimeout(()=>{
-        if(resultado){
-            resolve("Deu tudo certo")
-        }
-        else
-        {
-            reject("Deu tudo errado")
-        }
-    },tempo)
+btn_promessa.addEventListener("click", (evt) => {
+    numero.innerHTML = "Processando..."
+    console.log(promessa())
+        // .then((retorno) => {
+        //     numero.innerHTML = "Deu tudo certo"
+        //     numero.classList.remove("erro")
+        //     numero.classList.add("ok")
+        // })
+        // .catch((retorno) => {
+        //     numero.innerHTML = "Deu tudo certo"
+        //     numero.classList.add("erro")
+        //     numero.classList.remove("ok")
+        // })
 })
 
-promise.then((retorno)=>{
-    numero.innerHTML = retorno
-    numero.classList.remove("erro")
-    numero.classList.add("ok")
-})
+const promessa = () => {
+    let p = new Promise((resolve, reject) => {
+        let resultado = false
+        let tempo = 3000
 
-promise.catch((retorno)=>{
-    numero.innerHTML = retorno
-    numero.classList.add("erro")
-    numero.classList.remove("ok")
-})
+        setTimeout(() => {
+            if (resultado) {
+                resolve("Deu tudo certo")
+                numero.innerHTML = "Deu tudo certo"
+                numero.classList.remove("erro")
+                numero.classList.add("ok")
+            }
+            else {
+                reject("Deu tudo errado")
+                numero.innerHTML = "Deu tudo certo"
+                numero.classList.add("erro")
+                numero.classList.remove("ok")
+            }
+        }, tempo)
+    })
+    return p
+}
 
-numero.innerHTML = "Processando..."
+numero.innerHTML = "Esperando"
