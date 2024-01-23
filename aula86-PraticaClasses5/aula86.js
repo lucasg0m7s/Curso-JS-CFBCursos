ecdc32876170690ee21c10b0c5ce48d60ee431ea
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-23 01:06:39 -0300

    Concluir aula86

--- aula86-PraticaClasses5/aula86.js
+++ aula86-PraticaClasses5/aula86.js
@@ -16,7 +16,7 @@ class Bola{
         this.g = Math.floor(Math.random()*255)
         this.b = Math.floor(Math.random()*255)
         this.px = Math.floor(Math.random()*(larguraPalco - this.tam))
-        this.py = Math.floor(Math.random()*(alturaPalcoPalco - this.tam))
+        this.py = Math.floor(Math.random()*(alturaPalco - this.tam))
         this.velx = Math.floor(Math.random()*2) + 0,5
         this.vely = Math.floor(Math.random()*2) + 0,5
         this.dirx = Math.floor(Math.random()*10) > 5 ? 1 : -1
@@ -51,12 +51,34 @@ class Bola{
         const div = document.createElement("div")
         div.setAttribute("id", this.id)
         div.setAttribute("class", "bola")
-        div.setAttribute("style", `left:${this.px};top:${this.py};width:${this.tam};height:${this.tam};background-color:rgb(${this.r},${this.g},${this.b})`)
+        div.setAttribute("style", `left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`)
         this.palco.appendChild(div)
     }
 
-    controlar = ()=>{
+    controle_bordas = ()=>{
+        if(this.px + this.tam >= larguraPalco){
+            this.dirx = -1
+        }
+        else if(this.px <= 0){
+            this.dirx = 1
+        }
 
+        if(this.py + this.tam >= alturaPalco){
+            this.diry = -1
+        }
+        else if(this.py <= 0){
+            this.diry = 1
+        }
+    }
+
+    controlar = ()=>{
+        this.controle_bordas()
+        this.px += this.dirx * this.velx
+        this.py += this.diry * this.vely
+        this.eu.setAttribute("style", `left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`)
+        if(this.px > larguraPalco || this.py > alturaPalco){
+            this.remover()
+        }
     }
 }
 
@@ -68,12 +90,12 @@ window.addEventListener("resize",(evt)=>{
 btn_add.addEventListener("click",(evt)=>{
     const qtde = txt_qtde.value
     for(let i = 0; i < qtde; i++){
-        // Instanciar novas bolinhas
+        bolas.push(new Bola(bolas, palco))
     }
 })
 
 btn_remover.addEventListener("click",(evt)=>{
     bolas.map((b)=>{
-        // Remover a bolinha
+        b.remover()
     })
 })
