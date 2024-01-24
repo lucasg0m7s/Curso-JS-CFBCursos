08ecdca1db8215a74d2d05a4793f1472b8412da8
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-24 20:35:28 -0300

    Criar aula93

---
+++ aula93-Modulos2/aula93.js
@@ -0,0 +1,7 @@
+import { cursos } from "./cursos.js";
+import { carros } from "./cursos.js";
+
+console.log("servidor rodando")
+console.log(cursos)
+console.log(carros)
+
---
+++ aula93-Modulos2/cursos.js
@@ -0,0 +1,6 @@
+const cursos = ["Javascript", "HTML", "CSS", "Arduino", "Rapspberry", "C++", "Python", "C#"]
+
+const carros = ["Polo", "T-Cross", "Golf", "Onix", "Cruze", "Argo", "Cronos"]
+
+export {cursos}
+export {carros}
