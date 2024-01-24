1103b6afb581289af50a46eab10a5e8f078045f7
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-23 23:47:55 -0300

    Concluir aula89

--- aula89-CriandoPDF/aula89.js
+++ aula89-CriandoPDF/aula89.js
@@ -1,20 +1,24 @@
-const btn_alert = document.getElementById("btn_alert")
-const btn_confirm = document.getElementById("btn_confirm")
-const btn_prompt = document.getElementById("btn_prompt")
+const btn_imp = document.getElementById("btn_imp");
 
-btn_alert.addEventListener("click", (evt)=>{
-    alert("Olá como você está?")
-})
+btn_imp.addEventListener("click", (evt)=>{
+    const conteudo = document.getElementById("tabela").innerHTML;
+    
+    let estilo = "<style>";
+    estilo += "table {width: 100%; font:25px Calibri;}";
+    estilo += "table, th, td {border: solid 2px #888; border-collapse: collapse;";
+    estilo += "padding: 4px 8px; text-align: center;}";
+    estilo += "</style>";
 
-btn_confirm.addEventListener("click", (evt)=>{
-    confirm("Você está aprendendo muito?") ? console.log("Botão OK pressionado") : console.log("Botão Cancelar pressionado")
-})
+    const win = window.open('', '', 'height = 700, width = 700');
+    
+    win.document.write('<html><head>');
+    win.document.write('<title>CFB Cursos</title>');
+    win.document.write(estilo);
+    win.document.write('</head>');
+    win.document.write('<body>');
+    win.document.write(conteudo);
+    win.document.write('</body></html>');
 
-btn_prompt.addEventListener("click", (evt)=>{
-    const nome = prompt("Digite seu nome:", "Digite seu nome aqui")
-    if(nome == null){
-        console.log("Botão CANCELAR pressionado")
-    }else{
-        console.log("Nome digitado: " + nome)
-    }
+    win.print();
+    win.close()
 })
