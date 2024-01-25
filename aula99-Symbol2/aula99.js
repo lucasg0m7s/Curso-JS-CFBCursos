1a6871ca611778d6821596332e24ecb60697cdd2
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-25 19:28:24 -0300

    Criar aula99

---
+++ aula99-Symbol2/aula99.js
@@ -0,0 +1,12 @@
+const s1 = Symbol()
+const s2 = Symbol()
+const s3 = Symbol.for("bruno")
+const s4 = Symbol.for("canal")
+
+console.log(s1)
+console.log(s2)
+
+console.log(s3 == s4)
+
+console.log(Symbol.keyFor(s3))
+
