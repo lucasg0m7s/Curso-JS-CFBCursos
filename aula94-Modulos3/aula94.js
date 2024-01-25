520228623d0e186d7c64da1d66d198c40e066042
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-24 21:49:10 -0300

    Criar aula94

---
+++ aula94-Modulos3/aula94.js
@@ -0,0 +1,6 @@
+import getTodosCursos, {cursos, getCurso} from "./cursos2.js";
+
+console.log(cursos)
+console.log(getTodosCursos())
+console.log(getCurso(1))
+
---
+++ aula94-Modulos3/cursos2.js
@@ -0,0 +1,16 @@
+const cursos = ["Javascript", "HTML", "CSS", "Arduino", "Rapspberry", "C++", "Python", "C#"]
+
+// const getTodosCursos = ()=>{
+//     return cursos
+// }
+
+// Apenas um export default
+export default function getTodosCursos(){
+    return cursos
+}
+
+function getCurso(i_curso){
+    return cursos[i_curso]
+}
+
+export {cursos, getCurso}
