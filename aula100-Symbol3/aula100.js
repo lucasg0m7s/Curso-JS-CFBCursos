30dd91a54be88e4dddcfd3296d0f8756f1961e26
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-25 19:58:58 -0300

    Criar aula100

---
+++ aula100-Symbol3/aula100.js
@@ -0,0 +1,22 @@
+class Jogador{
+    constructor(nome){
+        this.id = Symbol()
+        this.nome = nome
+    }
+}
+
+let jogadores = [new Jogador("j1"), new Jogador("j2"), new Jogador("j3"), new Jogador("j4"), new Jogador("j1"), new Jogador("j3")]
+
+let s = []
+
+let s_jogadores = jogadores.filter((j)=>{
+    return j.nome == "j1"
+})
+
+s = s_jogadores.map((j)=>{
+    return j.id
+})
+
+console.log(jogadores)
+console.log(s)
+console.log(s_jogadores)
