7188184860f513dfb03b5afb9badfb5dea8c88d5
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-26 22:08:58 -0300

    Criar aula102

---
+++ aula102-ManipularStrings2/aula102.js
@@ -0,0 +1,14 @@
+let nome = new String("Bruno Pinho Campos")
+let canal = new String("CFBCursos")
+
+console.log(nome)
+
+console.log(nome.charAt(0)) // Retorna o caractere de acordo com a posição indicada
+
+console.log(nome.charCodeAt(0)) // Retorna código do caractere de acordo com a posição indicada
+
+console.log(nome.concat(canal)) // Retorna a concatenação de duas strings
+
+console.log(nome.indexOf("o")) // Retorna a posição do primeiro caractere indicado. Caso não encontre, retorna -1.
+
+console.log(nome.lastIndexOf("o")) // Retorna a posição do ultimo caractere indicado. Caso não encontre, retorna -1.
