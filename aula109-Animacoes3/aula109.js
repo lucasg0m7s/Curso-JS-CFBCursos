7c778a2df3333920412f6f92e4dda1f8617fe1ab
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-29 20:25:28 -0300

    Concluir aula109

--- aula109-Animacoes3/aula109.js
+++ aula109-Animacoes3/aula109.js
@@ -12,20 +12,21 @@ let anima = null
 
 const move = (dir)=>{
     carro.style.left = parseInt(carro.style.left) + (10 * dir) + "px"
+    anima = setTimeout(move, 20, dir)
 }
 
 btn_parar.addEventListener("click", ()=>{
-    clearInterval(anima)
+    clearTimeout(anima)
 })
 
 btn_esquerda.addEventListener("click", ()=>{
-    clearInterval(anima)
-    anima = setInterval(move, 20, -1)
+    clearTimeout(anima)
+    move(-1)
 })
 
 btn_direita.addEventListener("click", ()=>{
-    clearInterval(anima)
-    anima = setInterval(move, 20, 1)
+    clearTimeout(anima)
+    move(1)
 })
 
 window.onload = init
