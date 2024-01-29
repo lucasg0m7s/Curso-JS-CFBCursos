05f7e4bcb72df34460ae0d9c2115f7fad3b0b0f0
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-29 20:16:17 -0300

    Concluir aula108

--- aula108-Animacoes2/aula108.js
+++ aula108-Animacoes2/aula108.js
@@ -1,20 +1,31 @@
 const carro = document.getElementById("carro")
 const btn_esquerda = document.getElementById("btn_esquerda")
 const btn_direita = document.getElementById("btn_direita")
+const btn_parar = document.getElementById("btn_parar")
+
 
 const init = ()=>{
     carro.style = "position: relative; left: 0px;"
 }
 
+let anima = null
+
+const move = (dir)=>{
+    carro.style.left = parseInt(carro.style.left) + (10 * dir) + "px"
+}
+
+btn_parar.addEventListener("click", ()=>{
+    clearInterval(anima)
+})
+
 btn_esquerda.addEventListener("click", ()=>{
-    carro.style.left = parseInt(carro.style.left) - 10 + "px"
+    clearInterval(anima)
+    anima = setInterval(move, 20, -1)
 })
 
 btn_direita.addEventListener("click", ()=>{
-    let pos = parseInt(carro.style.left)
-    pos += 10
-    carro.style = `position: relative; left: ${pos}px;`
+    clearInterval(anima)
+    anima = setInterval(move, 20, 1)
 })
-
 
 window.onload = init
