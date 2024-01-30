232bb95358c7117ca393f0eb8c343c9d40d2daab
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-30 14:00:54 -0300

    Concluir aula114

--- aula114-Desestruturacao/aula114.js
+++ aula114-Desestruturacao/aula114.js
@@ -1,28 +1,12 @@
-const q1 = document.getElementById("q1")
-const q2 = document.getElementById("q2")
-const posx = document.getElementById("posx")
-const posy = document.getElementById("posy")
-const largura = document.getElementById("largura")
-const altura = document.getElementById("altura")
-
-q1.accessKey = "b"
-q2.accessKey = "n"
-
-console.log("q1: " + q1.accessKey)
-console.log("q2: " + q2.accessKey)
-
-const info = (el)=>{
-    let DOMRECT = el.getBoundingClientRect()
-    posx.innerHTML = `posx: ${DOMRECT.x}`
-    posy.innerHTML = `posy: ${DOMRECT.y}`
-    largura.innerHTML = `largura: ${DOMRECT.width}`
-    altura.innerHTML = `altura: ${DOMRECT.height}`
+let numeros = ()=>{
+    return [10,20,30,40]
 }
 
-q1.addEventListener("click", (evt)=>{
-    info(evt.target)
-})
+let[a,b,c,d] = numeros()
 
-q2.addEventListener("click", (evt)=>{
-    info(evt.target)
-})
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+
