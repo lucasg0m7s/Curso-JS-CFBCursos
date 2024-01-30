4b40ee6cebe56260ba25ce06047ae7ea20dc2649
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-30 14:37:56 -0300

    Concluir aula116

--- aula116-Geolocalizacao/aula116.js
+++ aula116-Geolocalizacao/aula116.js
@@ -1,39 +1,22 @@
-// Associa os valores retornados de um objeto ou de um array em variáveis distintas
+const long = document.getElementById("long")
+const lati = document.getElementById("lati")
 
-let numeros = [10,20,30,40,60,70,80,90]
-let[a,b,c,...d] = numeros
-
-console.log(a)
-console.log(b)
-console.log(c)
-console.log(d)
-
-let obj = {
-    nome: "Bruno",
-    canal: "CFBCursos",
-    curso: "JS"
+if(navigator.geolocation)
+{
+    navigator.geolocation.getCurrentPosition(mostrarLocalizacao, erroLocalizacao)
+}
+else
+{
+    console.log("Geolocalização não suportada")
 }
 
-let{nome, canal} = obj
-
-console.log(nome)
-console.log(canal)
-
-const cores = ()=>{
-    return ["verde", "amarelo", "azul", "branco"]
+function mostrarLocalizacao(pos){
+    long.innerHTML = "Longitude: " + pos.coords.longitude
+    lati.innerHTML = "Latitude: " + pos.coords.latitude
 }
 
-let[c1,c2,,c3] = cores()
-
-console.log(c1)
-console.log(c2)
-console.log(c3)
-
-let texto = "Curso de JS"
-
-let [p1, p2 ,p3] = texto.split(" ")
-console.log(p1)
-console.log(p2)
-console.log(p3)
+function erroLocalizacao(pos){
+    console.log("Erro ao obter a localização")
+}
 
 
