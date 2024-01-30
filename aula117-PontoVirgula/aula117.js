b063bfc4b52843d1967df78aa6484259d92c2238
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-30 20:45:40 -0300

    Concluir aula117

--- aula117-PontoVirgula/aula117.js
+++ aula117-PontoVirgula/aula117.js
@@ -1,22 +1,7 @@
-const long = document.getElementById("long")
-const lati = document.getElementById("lati")
+let i = null
 
-if(navigator.geolocation)
-{
-    navigator.geolocation.getCurrentPosition(mostrarLocalizacao, erroLocalizacao)
-}
-else
-{
-    console.log("Geolocalização não suportada")
-}
+for(i=0;i<10;i++);
 
-function mostrarLocalizacao(pos){
-    long.innerHTML = "Longitude: " + pos.coords.longitude
-    lati.innerHTML = "Latitude: " + pos.coords.latitude
-}
-
-function erroLocalizacao(pos){
-    console.log("Erro ao obter a localização")
-}
+console.log(i)
 
 
