ce5c2991831a3055d12b5682aba251fa357723bb
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-30 14:03:05 -0300

    Criar aula115

---
+++ aula115-Desestruturacao2/aula115.js
@@ -0,0 +1,14 @@
+// Associa os valores retornados de um objeto ou de um array em variáveis distintas
+
+let numeros = ()=>{
+    return [10,20,30,40]
+}
+
+let[a,b,c,d] = numeros()
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+
