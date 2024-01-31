8aa11240362441934cb164521cf69d2fadaee460
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-31 20:21:52 -0300

    Concluir aula122

--- aula122-Fetch1/aula122.js
+++ aula122-Fetch1/aula122.js
@@ -1,25 +1,7 @@
-const f_texto = document.getElementById("f_texto")
-const p_texto = document.getElementById("p_texto")
-const btn_texto = document.getElementById("btn_texto")
+const endpoint = "https://7f6b143f-a8b1-4848-ba5e-1c98292b71a2-00-u4mlvujqih2e.kirk.replit.dev/"
 
-btn_texto.addEventListener("click", evt => {
-    
+fetch(endpoint)
+.then(res => res.json())
+.then(dados =>{
+    console.log(dados)
 })
-
-let num = 10
-let curso = "Javascript"
-// window.localStorage.setItem("numero", num)
-// localStorage.setItem("nome", "Bruno")
-// localStorage.setItem("canal", "CFBCursos")
-// localStorage.setItem("curso", curso)
-// alert(localStorage.length)
-// alert(localStorage.getItem(localStorage.key(0)))
-// alert(localStorage.getItem("nome"))
-// alert(localStorage.getItem("canal"))
-// alert(localStorage.getItem("curso"))
-localStorage.clear()
-
-sessionStorage.setItem("nome", "Bruno")
-sessionStorage.setItem("canal", "CFBCursos")
-sessionStorage.setItem("curso", curso)
-
