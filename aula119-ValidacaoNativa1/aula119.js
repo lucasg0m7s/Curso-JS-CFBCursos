405c08d776a097fc0e7c04e699b2f59fa56e3e02
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-30 21:15:29 -0300

    Criar aula119

---
+++ aula119-ValidacaoNativa1/aula119.js
@@ -0,0 +1,34 @@
+// let i = null; i = 0
+// console.log(i)
+
+//-------------------------
+
+// let i = null
+// for(i=0;i<10;i++);
+// console.log(i)
+
+//-------------------------
+
+// Toda vez que na próxima linha for iniciar uma instrução que comece com colchetes ou parenteses, preciso utilizar o ';'
+// let canal = "CFB Cursos";
+// ['j', 'a'].forEach(letra => console.log(letra))
+
+//-------------------------
+
+// let n1 = [1,2]
+// let n2 = [3,4];
+// [n1,n2].map(v => console.log(v))
+
+//-------------------------
+
+// let n = null;
+// (n = 10)
+// console.log(n)
+
+//-------------------------
+
+// let numeros = [1, 2, 3];
+// [n1, n2, n3] = numeros
+// console.log(n1,n2,n3)
+
+
