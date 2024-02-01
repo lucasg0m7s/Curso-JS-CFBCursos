91af32b950d2a0bcdf69a84b8a5e69549e38a604
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-01-31 23:41:00 -0300

    Concluir aula124

--- aula124-Fetch3/aula124.js
+++ aula124-Fetch3/aula124.js
@@ -1,6 +1,8 @@
 const p_temp = document.getElementById("p_temp")
 const p_nivel = document.getElementById("p_nivel")
 const p_press = document.getElementById("p_press")
+const btn_texto = document.getElementById("btn_texto")
+
 
 const obterDados = ()=>{
     const endpoint = "https://7f6b143f-a8b1-4848-ba5e-1c98292b71a2-00-u4mlvujqih2e.kirk.replit.dev/"
@@ -16,4 +18,24 @@ const obterDados = ()=>{
     })
 }
 
-let intervalo = setInterval(obterDados, 10000)
+// let intervalo = setInterval(obterDados, 1000)
+
+let dados = {
+    nome: "Bruno",
+    canal: "CFBCursos",
+    curso: "JS"
+}
+
+const gravarDados = ()=>{
+    const endpoint = "https://7f6b143f-a8b1-4848-ba5e-1c98292b71a2-00-u4mlvujqih2e.kirk.replit.dev/"
+    fetch(endpoint, {
+        method: "POST",
+        body: JSON.stringify(dados)
+    })
+    .then(res => res.json())
+    .then(ret => {
+        console.log(ret)
+    })
+}
+
+btn_texto.addEventListener("click", evt => gravarDados())
