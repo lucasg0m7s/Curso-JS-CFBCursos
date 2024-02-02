847b1eda94574bf443ecb9cedcef38fe48ce4a08
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-02-01 23:52:09 -0300

    Concluir aula125

--- aula125-Fetch4/aula125.js
+++ aula125-Fetch4/aula125.js
@@ -28,6 +28,7 @@ let dados = {
 
 const gravarDados = ()=>{
     const endpoint = "https://7f6b143f-a8b1-4848-ba5e-1c98292b71a2-00-u4mlvujqih2e.kirk.replit.dev/"
+    
     fetch(endpoint, {
         method: "POST",
         body: JSON.stringify(dados)
