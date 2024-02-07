898d67de254e32a77150298e1a08ed346a3bdb0a
Lucas Gomes Ferreira <147620633+lucasg0m7s@users.noreply.github.com>
2024-02-07 20:48:10 -0300

    Concluir aula129

--- aula129-Timestamp1/aula129.js
+++ aula129-Timestamp1/aula129.js
@@ -1,11 +1,13 @@
-const texto = document.getElementById("texto")
+const timer = document.getElementById("timer")
 
-const endpoint = "texto.txt"
+const tmpini = Date.now()
 
-fetch(endpoint)
-.then(res => res.json())
-.then(res => {
-    console.log(res)
-    console.log(res.canal)
-    console.log(res.curso)
-})
+const contador = ()=>{
+    const tmpatual = Date.now()
+    let cont = tmpatual - tmpini
+    let seg = Math.floor((tmpatual - tmpini) / 1000)
+    console.log(seg)
+}
+
+setInterval(contador, 1000)
+contador()
